088775a11aa5cc02892600ced4dba8ba7f8e70d9
Dave Curran <curran.dave.m@gmail.com>
2017-02-08 15:16:07 -0500

    wip

--- metab_models.py
+++ metab_models.py
@@ -9,16 +9,78 @@ mtb.summary(): rates mtb is being produced and used in the current FBA.
 """
 from read_excel import read_excel
 
+# # #  Options
+verbose = True # True or False.
+
 def basic_stats(model):
-    print('The model %s consists of %i reactions, %i metabolites, and %i genes.' % (model, len(model.reactions), len(model.metabolites), len(model.genes)))
-def find_orphans(model):
-    mtbs = {}
+    descrip_str = '\n\nBasic stats for model: %s' % model
+    print('%s\n%s' % (descrip_str, '='*len(descrip_str.strip())))
+    print('Contains: %i reactions, %i metabolites, and %i genes.' % (len(model.reactions), len(model.metabolites), len(model.genes)))
+    topology_analysis(model)
+    end_str = '\nEnd of basic stats for model: %s' % model
+    print('%s\n%s' % (end_str, '-'*len(end_str.strip())))
+def topology_analysis(model):
+    mtbs, blocked = {}, []
     for rxn in model.reactions:
         r_id = rxn.id
+        forward = True if rxn.upper_bound > 0 else False
+        reverse = True if rxn.lower_bound < 0 else False
+        if forward == reverse == False:
+            blocked.append(r_id)
+            continue
         for mtb, coef in rxn.metabolites.items():
-            if coef > 0:
-                # produced.
-                pass
+            m_id = mtb.id
+            prd, rct = False, False
+            if forward == reverse == True:
+                prd, rct = True, True
+            elif coef > 0: # is a product of this reaction.
+                if forward:
+                    prd = True
+                elif reverse:
+                    rct = True
+            elif coef < 0: # is a reactant of this reaction.
+                if reverse:
+                    prd = True
+                elif forward:
+                    rct = True
+            else:
+                print('\nWarning: in reaction %s, metabolite %i has unknown coefficient "%s"' % (r_id, m_id, coef))
+                continue
+            if prd:
+                mtbs.setdefault(m_id, {'product':[], 'reactant':[]})['product'].append(r_id)
+            if rct:
+                mtbs.setdefault(m_id, {'product':[], 'reactant':[]})['reactant'].append(r_id)
+    print('\nBlocked reactions: %i allowing no flux.' % len(blocked))
+    sprfls = set(m.id for m in model.metabolites) - set(mtbs.keys())
+    if len(sprfls) == 0:
+        sprfls_mtb_str = '0.'
+    else:
+        sprfls_mtb_str = '%i described but not part of any reaction.' % len(sprfls)
+    print('\nSuperfluous metabolites: %s' % sprfls_mtb_str)
+    not_gen, not_used = [], []
+    for mtb, data in mtbs.items():
+        if len(data['product']) == 0:
+            not_gen.append(mtb)
+        if len(data['reactant']) == 0:
+            not_used.append(mtb)
+    if len(not_gen) == 0:
+        unavail_metab_str = '0. All metabolites produced by reactions.'
+    else:
+        unavail_metab_str = '%i metabolites used in reactions, but not produced by any.' % len(not_gen)
+        if verbose:
+            not_gen.sort()
+            unavail_metab_str += '\n\t%s' % (', '.join(not_gen))
+    print('\nUnavailable metabolites: %s' % unavail_metab_str)
+    if len(not_used) == 0:
+        unused_mtb_str = '0. All metabolites used in reactions.'
+    else:
+        unused_mtb_str = '%i metabolites produced in reactions, but not used by any.' % len(not_used)
+        if verbose:
+            not_used.sort()
+            unused_mtb_str += '\n\t%s' % (', '.join(not_used))
+    print('\nUnused metabolites: %s' % unused_mtb_str)
+
+# Shadow prices.
 
 
 ov_model_file = 'model_o_vol.xlsx'
@@ -29,5 +91,3 @@ bm = read_excel(bm_model_file)
 
 basic_stats(ov)
 basic_stats(bm)
-
-find_orphans(bm)
