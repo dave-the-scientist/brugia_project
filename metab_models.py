38d6e6964a355d372cf6b4638cafb0c86b9b13d0
Dave Curran <curran.dave.m@gmail.com>
2017-02-10 15:01:01 -0500

    wip

--- metab_models.py
+++ metab_models.py
@@ -80,7 +80,8 @@ def topology_analysis(model):
             unused_mtb_str += '\n\t%s' % (', '.join(not_used))
     print('\nUnused metabolites: %s' % unused_mtb_str)
 
-### I have 2 active overview.doc files. Have to resolve their differences on the PC, then fix the dir structure.
+def compare_models(m1, m2):
+    pass
 
 
 ov_model_file = 'model_o_vol.xlsx'
